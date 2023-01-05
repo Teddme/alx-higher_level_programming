5109346c94b8c7e4020ba79001f2cf866eb40daa
Teddme <114336238+Teddme@users.noreply.github.com>
2023-01-05 13:04:52 +0000

    Spelling error

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, ' is positive')
+    print(number, ' is positive')
 if number < 0:
     print(number, ' is negative')
 if number == 0:
