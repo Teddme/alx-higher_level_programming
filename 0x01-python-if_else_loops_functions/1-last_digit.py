c251a80b1b534193116a1a1d9f687d60e4f1bc41
Teddme <114336238+Teddme@users.noreply.github.com>
2023-01-06 12:22:20 +0000

    Error handling

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 st = str(number)
 num = int(st[len(st)-1])
-if number < 0:
-    print('Last digit of', st, 'is', -num, 'and is less than 6 and not 0')
-elif num == 0:
-    print('Last digit of', st, 'is', num, 'and is 0')
-elif num > 5:
-    print('Last digit of', st, 'is', num, 'and is greater than 5')
-elif num <= 5:
-    print('Last digit of', st, 'is', num, 'and is less than 6 and not 0')
-else:
-    print('TypeError')
+if(abs(number)):
+    if number < 0:
+        print('Last digit of', st, 'is', -num, 'and is less than 6 and not 0')
+    elif num == 0:
+        print('Last digit of', st, 'is', num, 'and is 0')
+    elif num > 5:
+        print('Last digit of', st, 'is', num, 'and is greater than 5')
+    elif num <= 5:
+        print('Last digit of', st, 'is', num, 'and is less than 6 and not 0')
+    else:
+        print('TypeError')
