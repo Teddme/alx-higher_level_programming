580bca19312fba18bb90dccb94a3097af8d4b21a
Teddme <114336238+Teddme@users.noreply.github.com>
2023-01-06 12:04:47 +0000

    8 and 9 updates

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(str):
+    Ch_prt =""
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
+        Ch_prt = Ch_prt + i
+    print("{:s}".format(Ch_prt), end="")
     print()
