6acbead3c999e9e5b95d9317ec20cfb51123e517
Teddme <114336238+Teddme@users.noreply.github.com>
2023-01-06 10:53:38 +0000

    Spacing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 st = str(number)
 num = int(st[len(st)-1])
 if num == 0:
-    print('Last digit of ', st, ' is ' ,num ,' and is ', num)
+    print('Last digit of', st, 'is' ,num ,'and is', num)
 if num > 5:
-    print('Last digit of ', st, ' is ' ,num ,' and is greater than 5')
+    print('Last digit of', st, 'is' ,num ,'and is greater than 5')
 if num <=5:
-    print('Last digit of ', st, ' is ' ,num ,' and is less than 6 and not 0')
+    print('Last digit of', st, 'is' ,num ,'and is less than 6 and not 0')
