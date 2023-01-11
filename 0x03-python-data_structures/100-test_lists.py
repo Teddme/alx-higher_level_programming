6d8ae1327bcc178ef6086c0f2f4f33c166185bb8
Teddme <114336238+Teddme@users.noreply.github.com>
2023-01-11 04:30:15 +0000

    File access and Pycodestyle adjusted

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+lb = ['hello', 'World']
+lib.print_python_list_info(lb)
+del lb[1]
+lib.print_python_list_info(lb)
+lb = lb + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(lb)
+lb = []
+lib.print_python_list_info(lb)
+lb.append(0)
+lib.print_python_list_info(lb)
+lb.append(1)
+lb.append(2)
+lb.append(3)
+lb.append(4)
+lib.print_python_list_info(lb)
+lb.pop()
+lib.print_python_list_info(lb)
